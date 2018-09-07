7dda8afcd66ae46c88768c04255ab27ca201c04b
Maximilian Krome <maximilian.krome@student.uni-luebeck.de>
2018-09-08 01:25:39 +0200

    done

--- fetch_file.py
+++ fetch_file.py
@@ -1,23 +1,24 @@
 from urllib.request import urlopen
-import requests
 import json
 
-print("starting")
 
-response = requests.get("192.168.42.102")
-page_source = response.read()
+response = urlopen("http://192.168.42.102")
+page_source = response.read().decode("utf-8", "ignore")
 
+print(page_source)
 all_vessel = []
+
 for vessel in page_source.split(";"):
 	arr = vessel.split(",")
+	if arr[0] == '':
+		break
 	mmsi = int(arr[0])
-	lat = int(arr[1])
-	lon = int(arr[2])
+	lat = float(arr[1])
+	lon = float(arr[2])
 	all_vessel.append({"mmsi": mmsi, "latitude": lat, "longitude": lon})
 
-json_data = json.dumps(data)
+json_data = json.dumps(all_vessel)
 
 file_handle = open("data.json", "w")
 file_handle.write(json_data)
-
-print("done")
+file_handle.close()
