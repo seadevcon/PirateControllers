29904cb13711a7d1100856d6ed9e5d82b44b7f4d
MaBeKro <maximilian.krome@student.uni-luebeck.de>
2018-09-07 22:34:28 +0200

    fix

---
+++ fetch_file.py
@@ -0,0 +1,19 @@
+import urllib2
+import json
+
+response = urllib2.urlopen("192:168:42:102")
+page_source = response.read()
+
+all_vessel = []
+for vessel in page_source.split(";"):
+	arr = vessel.split(",")
+	mmsi = int(arr[0])
+	lat = int(arr[1])
+	lon = int(arr[2])
+	all_vessel.append({"mmsi": mmsi, "latitude": lat, "longitude": lon})
+
+json_data = json.dumps(data)
+
+file_handle = open("data.json", "w")
+file_handle.write(json_data)
+
