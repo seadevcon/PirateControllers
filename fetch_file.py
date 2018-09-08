5c29ef4c77a90744e5e7b55beb52d74091caf098
Maximilian Krome <maximilian.krome@student.uni-luebeck.de>
2018-09-08 02:10:08 +0200

    beautify

--- fetch_file.py
+++ fetch_file.py
@@ -17,8 +17,8 @@ for vessel in page_source.split(";"):
 	lon = float(arr[2])
 	all_vessel.append({"mmsi": mmsi, "latitude": lat, "longitude": lon})
 
-json_data = "var responses = " + json.dumps(all_vessel)
+json_data = "var responseShips = " + json.dumps(all_vessel)
 
-file_handle = open("data.json", "w")
+file_handle = open("data.js", "w")
 file_handle.write(json_data)
 file_handle.close()
